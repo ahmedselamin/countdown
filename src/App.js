12e6919bb8db8eb817d8aa6bb388df5b3d0bd8cb
unknown <eddy@Surface-02.ebanan.local>
2022-07-05 10:07:07 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [days, setDays] = useState();
+  const [hours, setHours] = useState();
+  const [minutes, setMinutes] = useState();
+  const [seconds, setSeconds] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const countdown = () => {
+    const endDate = new Date("March 22, 2023 00:00:00").getTime();
+    const today = new Date().getTime();
+
+    const timeDiff = endDate - today;
+
+    const seconds = 1000;
+    const minutes = seconds * 60;
+    const hours = minutes * 60;
+    const days = hours * 24;
+
+    let timeDays = Math.floor(timeDiff / days);
+    let timeHours = Math.floor((timeDiff % days) / hours);
+    let timeMinutes = Math.floor((timeDiff % hours) / minutes);
+    let timeSeconds = Math.floor((timeDiff % minutes) / seconds);
+
+    timeHours = timeHours < 10 ? "0" + timeHours : timeHours;
+    timeMinutes = timeMinutes < 10 ? "0" + timeMinutes : timeMinutes;
+    timeSeconds = timeSeconds < 10 ? "0" + timeSeconds : timeSeconds;
+
+    setDays(timeDays);
+    setHours(timeHours);
+    setMinutes(timeMinutes);
+    setSeconds(timeSeconds);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    setInterval(countdown, 1000);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isLoading ? (
+        <div className="loading">
+          <div className="spinner"></div>
+        </div>
+      ) : (
+        <section className="container">
+          <h1>Ramadan 2023 countdown</h1>
+          <div className="countdown">
+            <article>
+              <p> {days} </p>
+              <h3> days</h3>
+            </article>
+            <article>
+              <p> {hours} </p>
+              <h3> hours</h3>
+            </article>
+            <article>
+              <p> {minutes} </p>
+              <h3> minutes</h3>
+            </article>
+            <article>
+              <p> {seconds} </p>
+              <h3> seconds </h3>
+            </article>
+          </div>
+        </section>
+      )}
+    </>
   );
-}
+};
 
 export default App;
